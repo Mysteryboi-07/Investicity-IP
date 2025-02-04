f70feed86b7d41a0b1ac64a2ad427f744daa86a0
Mysteryboi-07 <ngkianghwee@gmail.com>
2025-02-04 10:25:26 +0800

    4/2/2025

---
+++ script.js
@@ -0,0 +1,6 @@
+const APIKEY = "6796fc41f9d2bb1866181e28";
+const BASE_URL = "https://investicity-c8aa.restdb.io/rest/investicity-data";
+
+document.addEventListener("DOMContentLoaded", function () {
+    initializeApp();
+  });
