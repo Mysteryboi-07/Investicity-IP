86e95bbda3e1c478895a7e0567d7a21a40052355
Mysteryboi-07 <ngkianghwee@gmail.com>
2025-02-12 22:22:18 +0800

    clean up

--- activity.js
+++ activity.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     const transactions = [
       { date: "Date", type: "Type", asset: "Asset", amount: "Amount" },
-      { date: "2025-02-24", type: "Sell", asset: "Toshi", amount: "$5,000" },
-      { date: "2025-02-23", type: "Buy", asset: "Vine", amount: "$8,000" },
-      { date: "2025-02-23", type: "Send", asset: "Fartboy", amount: "$57800" },
-      { date: "2025-02-22", type: "Buy", asset: "IguanaPasta", amount: "$1,543" },
-      { date: "2025-02-21", type: "Sell", asset: "Ethereum", amount: "$2,500" },
-      { date: "2025-02-20", type: "Buy", asset: "Bitcoin", amount: "$40,000" },
+      { date: "2025-02-09", type: "Send", asset: "Bitcoin", amount: "$4,320" },
+      { date: "2025-02-07", type: "Sell", asset: "Toshi", amount: "$5,000" },
+      { date: "2025-01-29", type: "Buy", asset: "Vine", amount: "$8,000" },
+      { date: "2025-01-26", type: "Send", asset: "Fartboy", amount: "$57800" },
+      { date: "2025-01-13", type: "Buy", asset: "IguanaPasta", amount: "$1,543" },
+      { date: "2025-01-12", type: "Sell", asset: "Ethereum", amount: "$2,500" },
+      { date: "2025-01-01", type: "Buy", asset: "Bitcoin", amount: "$40,000" },
     ];
   
     const transactionList = document.querySelector(".transaction-list");
